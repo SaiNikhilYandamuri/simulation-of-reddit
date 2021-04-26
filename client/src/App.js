a64f66b778707046e619f05119fe76025b6669c1
Alihussain Ladiwala <alihussain.ladiwala@sjsu.edu>
2021-04-25 22:59:54 -0700

    basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./components/NavBar/NavBar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+
+const DefaultContainer = () => (
+  <div>
+    <div>
+      <NavigationBar />
+
+      <Route path="/" exact component={Landing} />
+    </div>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route component={DefaultContainer} />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./Landing.css";
+
+export default function Landing() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div className="landing-bg"></div>
+    </React.Fragment>
+  );
+}
