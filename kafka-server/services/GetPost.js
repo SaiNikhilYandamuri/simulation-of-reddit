90456ba464d402da668b1ba594eea4b1f8aa9e72
karan221997 <karan.jariwala221997@gmail.com>
2021-05-02 10:06:56 -0700

    fetch post api written. merge and again clone for usermetadata

--- kafka-server/services/GetPost.js
+++ kafka-server/services/GetPost.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const Post = require("../model/Post");
+const User = require("../model/");
 
 async function handle_request(msg, callback) {
   // Need to implement images later
   console.log(msg);
   console.log(msg.userEmail);
+  resultList=[]
 
   if(msg.userEmail == undefined){
       console.log("hello user undefined");
-      Post.find(
+      User.find(
         {communityName:msg.communityName}).sort({"created_time":-1}).then((error, result) => {
           if (error) {
             callback(null, error);
@@ -19,7 +21,7 @@ async function handle_request(msg, callback) {
           }
         });
   }else if(msg.communityName == undefined){
-      console.log("hello community name undefined");
+      console.log("hello community name undefined; changes in this api");
       Post.find(
         {createdByEmail:msg.userEmail}).sort({"created_time":-1}).then((error, result) => {
           if (error) {
