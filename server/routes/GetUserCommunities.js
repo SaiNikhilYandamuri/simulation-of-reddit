5431b955bc998f64daed162632c2134cb9923c3b
Sai Nikhil Yandamuri <nikhi.yandamuri@gmail.com>
2021-05-11 13:35:37 -0700

    Fetch User Communities

---
+++ server/routes/GetUserCommunities.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const checkAuth = require("../utils/passport");
+var kafka = require("../kafka/client");
+const passport = require("passport");
+
+router.post(
+  "/getUserCommunities",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    console.log("Inside get post");
+    kafka.make_request("getUserCommunities", req.body, function (err, results) {
+      //add topic/changeeee
+      console.log("Inside get post topic");
+      if (err) {
+        console.log("Inside err");
+        res.json({
+          status: "error",
+          msg: "Error",
+        });
+        res.status(400).end();
+      } else {
+        console.log("Inside else", results);
+
+        res.status(200).send(results);
+      }
+    });
+  }
+);
+
+module.exports = router;
